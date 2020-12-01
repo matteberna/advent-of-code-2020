66081eaca2080e72fd9df08bf7142b8809ca3025
Matteo Bernardini <watteberna@gmail.com>
2020-12-01 07:49:10 +0100

    Solve the second part of day 1

--- Day1/Solution.cs
+++ Day1/Solution.cs
@@ -21,7 +21,8 @@ namespace Whiskee.AdventOfCode2020.Day1
             input.Close();
 
             int firstSolution = 0;
-            
+            int secondSolution = 0;
+
             foreach (int a in expenses)
             {
                 foreach (int b in expenses)
@@ -29,13 +30,20 @@ namespace Whiskee.AdventOfCode2020.Day1
                     if (a != b && a + b == 2020)
                     {
                         firstSolution = a * b;
-                        goto done;
+                    }
+
+                    foreach (int c in expenses)
+                    {
+                        if (a != c && b != c && a + b + c == 2020)
+                        {
+                            secondSolution = a * b * c;
+                        }
                     }
                 }
             }
-            
-            done:
+
             Console.WriteLine($"First solution: {firstSolution}");
+            Console.WriteLine($"Second solution: {secondSolution}");
         }
     }
 }
