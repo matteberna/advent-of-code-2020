7f5e0c9c7a12a651de5db65d9de3e46cb3b020b4
Matteo Bernardini <watteberna@gmail.com>
2020-12-01 07:44:28 +0100

    Solve the first part of day 1

---
+++ Day1/Solution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Whiskee.AdventOfCode2020.Day1
+{
+    internal class Solution
+    {
+        public static void Run()
+        {
+            var expenses = new List<int>();
+            string line;
+            
+            var input = new System.IO.StreamReader("Day1/input.txt");
+            while((line = input.ReadLine()) != null)
+            {
+                if (int.TryParse(line, out int expense))
+                {
+                    expenses.Add(expense);
+                }
+            }
+            input.Close();
+
+            int firstSolution = 0;
+            
+            foreach (int a in expenses)
+            {
+                foreach (int b in expenses)
+                {
+                    if (a != b && a + b == 2020)
+                    {
+                        firstSolution = a * b;
+                        goto done;
+                    }
+                }
+            }
+            
+            done:
+            Console.WriteLine($"First solution: {firstSolution}");
+        }
+    }
+}
---
+++ Launcher.cs
@@ -0,0 +1,10 @@
+﻿namespace Whiskee.AdventOfCode2020
+{
+    internal class Launcher
+    {
+        private static void Main()
+        {
+            Day1.Solution.Run();
+        }
+    }
+}
