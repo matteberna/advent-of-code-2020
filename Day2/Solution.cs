6a8b7788e7a7539944e3baba211d4ab253dc6b31
Matteo Bernardini <watteberna@gmail.com>
2020-12-02 07:30:48 +0100

    Solve the second part of day 2

--- Day2/Solution.cs
+++ Day2/Solution.cs
@@ -15,7 +15,8 @@ namespace Whiskee.AdventOfCode2020.Day2
             var entries = new List<MatchCollection>();
             string line;
             
-            int validPasswords = 0;
+            int validFirst = 0;
+            int validSecond = 0;
             
             var input = new System.IO.StreamReader("Day2/input.txt");
             while((line = input.ReadLine()) != null)
@@ -35,11 +36,26 @@ namespace Whiskee.AdventOfCode2020.Day2
                 int occurrences = password.Count(c => c == required);
                 if (occurrences >= min && occurrences <= max)
                 {
-                    validPasswords++;
+                    validFirst++;
                 }
             }
             
-            Console.WriteLine($"First solution: {validPasswords}");
+            // Second part
+            foreach (var entry in entries)
+            {
+                int index1 = int.Parse(entry[0].Value) - 1;
+                int index2 = int.Parse(entry[1].Value) - 1;
+                char required = char.Parse(entry[2].Value);
+                string password = entry[3].Value;
+                if (password[index1] == required ^ password[index2] == required)
+                {
+                    validSecond++;
+                }
+                
+            }
+            
+            Console.WriteLine($"First solution: {validFirst}");
+            Console.WriteLine($"Second solution: {validSecond}");
             
         }
         
