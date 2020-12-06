ad27c8d3fab428051865939ddb7f223cc22d9234
Matteo Bernardini <watteberna@gmail.com>
2020-12-06 06:35:45 +0100

    Solve the second part of day 6

--- CSharp/Day6.cs
+++ CSharp/Day6.cs
@@ -1,20 +1,25 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using static System.Environment;
 
 namespace Whiskee.AdventOfCode2020.CSharp
 {
     public class Day6 : Day
     {
+        private const string Answers = "abcdefghijklmnopqrstuvwxyz";
+        
         public override void Run()
         {
-            string[] groups = File.ReadAllText(@"data/day6.txt").Split(Environment.NewLine + Environment.NewLine);
+            string[] groups = File.ReadAllText(@"data/day6.txt").Split(NewLine + NewLine);
 
             // First part
             int anyCount = groups.Sum(g => new string(g.Where(char.IsLetter).ToArray()).Distinct().Count());
             Console.WriteLine($"First solution: {anyCount}");
-
+            
+            // Second part
+            int allCount = groups.Sum(g => Answers.Count(a => g.Split(NewLine).All(m => m.Contains(a))));
+            Console.WriteLine($"Second solution: {allCount}");
         }
     }
 }
