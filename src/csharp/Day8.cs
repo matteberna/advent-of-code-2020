338179c95d2488a5cc034cb2a6135d2d9ef019ea
Matteo Bernardini <watteberna@gmail.com>
2020-12-08 06:27:34 +0100

    Solve the second part of day 8

--- src/csharp/Day8.cs
+++ src/csharp/Day8.cs
@@ -41,7 +41,58 @@ namespace Whiskee.AdventOfCode2020
 
         public override object SolveSecond()
         {
-            return 0;
+            for (int i = 0; i < _instructions.Length; i++)
+            {
+                int? accumulator = TestBuild(i);
+                if (accumulator != null)
+                {
+                    return accumulator;
+                }
+            }
+            return null;
+        }
+
+        private int? TestBuild(int lineChanged)
+        {
+            _accumulator = 0;
+            _visited = new bool[_instructions.Length];
+            if (_instructions[lineChanged].StartsWith("acc"))
+            {
+                return null;
+            }
+            
+            for (int i = 0; i < _instructions.Length; i++)
+            {
+                string instruction = _instructions[i];
+                string operation = instruction.Substring(0, 3);
+                if (i == lineChanged)
+                {
+                    operation = operation switch
+                    {
+                        "nop" => "jmp",
+                        "jmp" => "nop",
+                        _ => operation
+                    };
+                }
+                int argument = int.Parse(instruction.Substring(5)) * (instruction[4] == '-'? -1 : 1);
+                if (_visited[i])
+                {
+                    return null;
+                }
+                _visited[i] = true;
+                switch (operation)
+                {
+                    case "acc":
+                        _accumulator += argument;
+                        break;
+                    case "jmp":
+                        i += argument;
+                        i -= 1;
+                        break;
+                }
+            }
+
+            return _accumulator;
         }
     }
 }
