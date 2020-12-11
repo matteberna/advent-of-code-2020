94a5a5086ec9469a21dec4651f0e8a39dad03bb8
Matteo Bernardini <watteberna@gmail.com>
2020-12-11 08:05:52 +0100

    Solve the second part of day 11

--- csharp/Solutions/Day11.cs
+++ csharp/Solutions/Day11.cs
@@ -34,14 +34,64 @@ namespace Whiskee.AdventOfCode2020.Solutions
                         }
                         
                         // Will someone sit here?
-                        if (prev.At[x, y] == 'L' && IsGoodSeat(prev, x, y))
+                        if (prev.At[x, y] == 'L' && IsGoodSeat(prev, x, y, false))
                         {
                             updated.At[x, y] = '#';
                             changes++;
                         }
                         
                         // Will someone leave this seat?
-                        else if (prev.At[x, y] == '#' && IsTooCrowded(prev, x, y))
+                        else if (prev.At[x, y] == '#' && IsTooCrowded(prev, x, y, 4, false))
+                        {
+                            updated.At[x, y] = 'L';
+                            changes++;
+                        }
+                    }
+                }
+
+                if (changes > 0)
+                {
+                    prev.CopyFrom(updated);
+                }
+                else
+                {
+                    break;
+                }
+            }
+            
+            return CountSeats(prev);
+        }
+        
+        public override object SolveSecond()
+        {
+            var prev = Toolkit.Map.CreateFrom(_baseMap);
+            var updated = Toolkit.Map.CreateFrom(prev);
+            int changes;
+            
+            while (true)
+            {
+                _round++;
+                changes = 0;
+                
+                for (int x = 0; x < prev.Width; x++)
+                {
+                    for (int y = 0; y < prev.Height; y++)
+                    {
+                        // Is this even a seat?
+                        if (prev.At[x, y] == '.')
+                        {
+                            continue;
+                        }
+                        
+                        // Will someone sit here?
+                        if (prev.At[x, y] == 'L' && IsGoodSeat(prev, x, y, true))
+                        {
+                            updated.At[x, y] = '#';
+                            changes++;
+                        }
+                        
+                        // Will someone leave this seat?
+                        else if (prev.At[x, y] == '#' && IsTooCrowded(prev, x, y, 5, true))
                         {
                             updated.At[x, y] = 'L';
                             changes++;
@@ -79,47 +129,101 @@ namespace Whiskee.AdventOfCode2020.Solutions
             return count;
         }
 
-        private static bool IsGoodSeat(Toolkit.Map map, int x, int y)
+        private static bool IsGoodSeat(Toolkit.Map map, int x, int y, bool extendSight)
         {
-            bool valid = true;
-            for (int cx = x - 1; cx <= x + 1; cx++)
+            // Simple version: just check which neighboring seats are occupied
+            if (!extendSight)
             {
-                for (int cy = y - 1; cy <= y + 1; cy++)
+                bool good = true;
+                for (int cx = x - 1; cx <= x + 1; cx++)
                 {
-                    // Is this neighboring seat occupied?
-                    if (cx >= 0 && cx < map.Width && cy >= 0 && cy < map.Height && map.At[cx, cy] == '#')
+                    for (int cy = y - 1; cy <= y + 1; cy++)
                     {
-                        valid = false;
+                        if (cx >= 0 && cx < map.Width && cy >= 0 && cy < map.Height && map.At[cx, cy] == '#')
+                        {
+                            good = false;
+                        }
                     }
                 }
-            }
 
-            return valid;
+                return good;
+            }
+            // Sight mode: check the diagonals
+            else
+            {
+                return CheckFirstCharacterSeen(map, -1, -1, x, y) is not '#'
+                       && CheckFirstCharacterSeen(map, -1, 0, x, y) is not '#'
+                       && CheckFirstCharacterSeen(map, -1, 1, x, y) is not '#'
+                       && CheckFirstCharacterSeen(map, 0, -1, x, y) is not '#'
+                       && CheckFirstCharacterSeen(map, 0, 1, x, y) is not '#'
+                       && CheckFirstCharacterSeen(map, 1, -1, x, y) is not '#'
+                       && CheckFirstCharacterSeen(map, 1, 0, x, y) is not '#'
+                       && CheckFirstCharacterSeen(map, 1, 1, x, y) is not '#';
+            }
+        }
+
+        private static char? CheckFirstCharacterSeen(Toolkit.Map map, int dirX, int dirY, int x, int y)
+        {
+            int ext = 0;
+                
+            while (true)
+            {
+                ext++;
+                int cx = x + dirX * ext;
+                int cy = y + dirY * ext;
+                
+                if (cx < 0 || cx >= map.Width || cy < 0 || cy >= map.Height)
+                {
+                    return null;
+                }
+
+                if (map.At[cx, cy] == '.')
+                {
+                    continue;
+                }
+
+                return map.At[cx, cy];
+            }
         }
         
-        private static bool IsTooCrowded(Toolkit.Map map, int x, int y)
+        private static bool IsTooCrowded(Toolkit.Map map, int x, int y, int threshold, bool extendSight)
         {
-            int crowd = 0;
-            for (int cx = x - 1; cx <= x + 1; cx++)
+            // Simple version: just check which neighboring seats are occupied
+            if (!extendSight)
             {
-                for (int cy = y - 1; cy <= y + 1; cy++)
+                int crowd = 0;
+                for (int cx = x - 1; cx <= x + 1; cx++)
                 {
-                    // Is this neighboring seat occupied?
-                    if (cx >= 0 && cx < map.Width && cy >= 0 && cy < map.Height && map.At[cx, cy] == '#')
+                    for (int cy = y - 1; cy <= y + 1; cy++)
                     {
-                        crowd++;
+                        // Is this neighboring seat occupied?
+                        if (cx >= 0 && cx < map.Width && cy >= 0 && cy < map.Height && map.At[cx, cy] == '#')
+                        {
+                            crowd++;
+                        }
                     }
                 }
+                return crowd >= threshold + 1;
+            }
+            // Sight mode: check the diagonals
+            else
+            {
+                int crowd = 0;
+                if (CheckFirstCharacterSeen(map, -1, -1, x, y) == '#') crowd++;
+                if (CheckFirstCharacterSeen(map, -1, 0, x, y) == '#') crowd++;
+                if (CheckFirstCharacterSeen(map, -1, 1, x, y) == '#') crowd++;
+                if (CheckFirstCharacterSeen(map, 0, -1, x, y) == '#') crowd++;
+                if (CheckFirstCharacterSeen(map, 0, 1, x, y) == '#') crowd++;
+                if (CheckFirstCharacterSeen(map, 1, -1, x, y) == '#') crowd++;
+                if (CheckFirstCharacterSeen(map, 1, 0, x, y) == '#') crowd++;
+                if (CheckFirstCharacterSeen(map, 1, 1, x, y) == '#') crowd++;
+                
+                return crowd >= threshold;
             }
 
             // Including the seat we're checking
-            return crowd >= 5;
+            
         }
 
-        public override object SolveSecond()
-        {
-            return null;
-        }
-        
     }
 }
