b5a8749507c2c7267160c193e43224f279694b11
Matteo Bernardini <watteberna@gmail.com>
2020-12-12 06:43:53 +0100

    Solve the second part of day 12

--- csharp/Solutions/Day12.cs
+++ csharp/Solutions/Day12.cs
@@ -9,10 +9,11 @@ namespace Whiskee.AdventOfCode2020.Solutions
         private static char _facing;
         private static int _x;
         private static int _y;
+        private static int _xWaypoint;
+        private static int _yWaypoint;
 
         public override void ReadInput(string content)
         {
-            _facing = 'E';
             _actions = new List<Action>();
             foreach (string data in content.SplitLines())
             {
@@ -27,15 +28,19 @@ namespace Whiskee.AdventOfCode2020.Solutions
 
         public override object SolveFirst()
         {
+            _x = 0;
+            _y = 0;
+            _facing = 'E';
+            
             foreach (var action in _actions)
             {
-                Move(action.Direction, action.Value);
+                MoveShip(action.Direction, action.Value);
             }
 
             return Math.Abs(_x) + Math.Abs(_y);
         }
 
-        private static void Move(char direction, int value)
+        private static void MoveShip(char direction, int value)
         {
             switch (direction)
             {
@@ -51,9 +56,9 @@ namespace Whiskee.AdventOfCode2020.Solutions
                 case 'W':
                     _x -= value;
                     break;
-                // Move forward in the current direction
+                // MoveShip forward in the current direction
                 case 'F':
-                    Move(_facing, value);
+                    MoveShip(_facing, value);
                     break;
                 // Turn left or right
                 case 'L':
@@ -92,7 +97,75 @@ namespace Whiskee.AdventOfCode2020.Solutions
 
         public override object SolveSecond()
         {
-            return null;
+            _x = 0;
+            _y = 0;
+            _facing = 'E';
+            _xWaypoint = 10;
+            _yWaypoint = -1;
+            
+            foreach (var action in _actions)
+            {
+                MoveWaypoint(action.Direction, action.Value);
+            }
+            
+            return Math.Abs(_x) + Math.Abs(_y);
+        }
+
+        private void MoveWaypoint(char direction, int value)
+        {
+            switch (direction)
+            {
+                case 'N':
+                    _yWaypoint -= value;
+                    break;
+                case 'S':
+                    _yWaypoint += value;
+                    break;
+                case 'E':
+                    _xWaypoint += value;
+                    break;
+                case 'W':
+                    _xWaypoint -= value;
+                    break;
+                // Move the ship toward the waypoint
+                case 'F':
+                    int fdx = _xWaypoint - _x;
+                    int fdy = _yWaypoint - _y;
+                    _x += fdx * value;
+                    _y += fdy * value;
+                    _xWaypoint += fdx * value;
+                    _yWaypoint += fdy * value;
+                    break;
+                // Turn left or right
+                case 'L':
+                    int timesLeft = value / 90;
+                    int ldx = _xWaypoint - _x;
+                    int ldy = _yWaypoint - _y;
+                    for (int i = 1; i <= timesLeft; i++)
+                    {
+                        int tldx = ldx;
+                        int tldy = ldy;
+                        ldx = tldy;
+                        ldy = -tldx;
+                    }
+                    _xWaypoint = _x + ldx;
+                    _yWaypoint = _y + ldy;
+                    break;
+                case 'R':
+                    int timesRight = value / 90;
+                    int rdx = _xWaypoint - _x;
+                    int rdy = _yWaypoint - _y;
+                    for (int i = 1; i <= timesRight; i++)
+                    {
+                        int trdx = rdx;
+                        int trdy = rdy;
+                        rdx = -trdy;
+                        rdy = trdx;
+                    }
+                    _xWaypoint = _x + rdx;
+                    _yWaypoint = _y + rdy;
+                    break;
+            }
         }
     }
     
