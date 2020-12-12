5027e48c83430dd4d9db6b2ad2c2b1a01c81b5c1
Matteo Bernardini <watteberna@gmail.com>
2020-12-12 07:39:24 +0100

    Rewrite both parts in a cleaner way

--- csharp/Solutions/Day12.cs
+++ csharp/Solutions/Day12.cs
@@ -1,177 +1,134 @@
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 
 namespace Whiskee.AdventOfCode2020.Solutions
 {
     public class Day12 : Day
     {
-        private static List<Action> _actions;
+        private static List<(char direction, int value)> _actions;
         private static char _facing;
-        private static int _x;
-        private static int _y;
-        private static int _xWaypoint;
-        private static int _yWaypoint;
+        private static Point _ship;
+        private static Point _waypoint;
 
         public override void ReadInput(string content)
         {
-            _actions = new List<Action>();
-            foreach (string data in content.SplitLines())
+            _actions = new List<(char direction, int value)>();
+            foreach (string action in content.SplitLines())
             {
-                _actions.Add(new Action()
-                {
-                    Direction = data[0],
-                    Value = int.Parse(data.Substring(1))
-                });
+                _actions.Add((action[0], int.Parse(action.Substring(1))));
             }
-            
         }
 
         public override object SolveFirst()
         {
-            _x = 0;
-            _y = 0;
-            _facing = 'E';
-            
-            foreach (var action in _actions)
-            {
-                MoveShip(action.Direction, action.Value);
-            }
-
-            return Math.Abs(_x) + Math.Abs(_y);
-        }
-
-        private static void MoveShip(char direction, int value)
-        {
-            switch (direction)
-            {
-                case 'N':
-                    _y -= value;
-                    break;
-                case 'S':
-                    _y += value;
-                    break;
-                case 'E':
-                    _x += value;
-                    break;
-                case 'W':
-                    _x -= value;
-                    break;
-                // MoveShip forward in the current direction
-                case 'F':
-                    MoveShip(_facing, value);
-                    break;
-                // Turn left or right
-                case 'L':
-                    int timesLeft = value / 90;
-                    for (int i = 1; i <= timesLeft; i++)
-                    {
-                        _facing = GetNextDirection(_facing, false);
-                    }
-                    break;
-                case 'R':
-                    int timesRight = value / 90;
-                    for (int i = 1; i <= timesRight; i++)
-                    {
-                        _facing = GetNextDirection(_facing, true);
-                    }
-                    break;
-            }
-        }
-
-        private static char GetNextDirection(char original, bool clockwise)
-        {
-            switch (original)
-            {
-                case 'N':
-                    return clockwise ? 'E' : 'W';
-                case 'E':
-                    return clockwise ? 'S' : 'N';
-                case 'S':
-                    return clockwise ? 'W' : 'E';
-                case 'W':
-                    return clockwise ? 'N' : 'S';
-            }
-
-            return 'X';
+            return FollowRoute(false);
         }
 
         public override object SolveSecond()
         {
-            _x = 0;
-            _y = 0;
-            _facing = 'E';
-            _xWaypoint = 10;
-            _yWaypoint = -1;
-            
-            foreach (var action in _actions)
-            {
-                MoveWaypoint(action.Direction, action.Value);
-            }
-            
-            return Math.Abs(_x) + Math.Abs(_y);
+            return FollowRoute(true);
         }
 
-        private void MoveWaypoint(char direction, int value)
+        private static int FollowRoute(bool usingWaypoint)
+        {
+            // Set the initial state
+            _ship = new Point(0, 0);
+            _waypoint = new Point(10, -1);
+            _facing = 'E';
+            
+            foreach ((char direction, int value) in _actions)
+            {
+                ExecuteManoeuver(direction, value, usingWaypoint);
+            }
+
+            return Math.Abs(_ship.X) + Math.Abs(_ship.Y);
+        }
+
+        private static void ExecuteManoeuver(char direction, int value, bool usingWaypoint)
         {
             switch (direction)
             {
                 case 'N':
-                    _yWaypoint -= value;
+                    if (usingWaypoint) _waypoint.Y -= value;
+                    else _ship.Y -= value;
                     break;
                 case 'S':
-                    _yWaypoint += value;
+                    if (usingWaypoint) _waypoint.Y += value;
+                    else _ship.Y += value;
                     break;
                 case 'E':
-                    _xWaypoint += value;
+                    if (usingWaypoint) _waypoint.X += value;
+                    else _ship.X += value;
                     break;
                 case 'W':
-                    _xWaypoint -= value;
+                    if (usingWaypoint) _waypoint.X -= value;
+                    else _ship.X -= value;
                     break;
-                // Move the ship toward the waypoint
                 case 'F':
-                    int fdx = _xWaypoint - _x;
-                    int fdy = _yWaypoint - _y;
-                    _x += fdx * value;
-                    _y += fdy * value;
-                    _xWaypoint += fdx * value;
-                    _yWaypoint += fdy * value;
+                    if (usingWaypoint)
+                    {
+                        // Move toward the waypoint, <value> times
+                        var adjustment = new Point((_waypoint.X - _ship.X) * value, (_waypoint.Y - _ship.Y) * value);
+                        _ship.Offset(adjustment);
+                        _waypoint.Offset(adjustment);
+                    }
+                    else
+                    {
+                        // Keep moving in the current _facing direction
+                        ExecuteManoeuver(_facing, value, false);
+                    }
                     break;
-                // Turn left or right
                 case 'L':
-                    int timesLeft = value / 90;
-                    int ldx = _xWaypoint - _x;
-                    int ldy = _yWaypoint - _y;
-                    for (int i = 1; i <= timesLeft; i++)
-                    {
-                        int tldx = ldx;
-                        int tldy = ldy;
-                        ldx = tldy;
-                        ldy = -tldx;
-                    }
-                    _xWaypoint = _x + ldx;
-                    _yWaypoint = _y + ldy;
-                    break;
                 case 'R':
-                    int timesRight = value / 90;
-                    int rdx = _xWaypoint - _x;
-                    int rdy = _yWaypoint - _y;
-                    for (int i = 1; i <= timesRight; i++)
+                    if (usingWaypoint)
                     {
-                        int trdx = rdx;
-                        int trdy = rdy;
-                        rdx = -trdy;
-                        rdy = trdx;
+                        // Rotate the waypoint around the ship
+                        int times = value / 90;
+                        var delta = new Point(_waypoint.X - _ship.X, _waypoint.Y - _ship.Y);
+                        for (int i = 1; i <= times; i++)
+                        {
+                            int temp = delta.X;
+                            switch (direction)
+                            {
+                                case 'L':
+                                    delta.X = delta.Y;
+                                    delta.Y = -temp;
+                                    break;
+                                case 'R':
+                                    delta.X = -delta.Y;
+                                    delta.Y = temp;
+                                    break;
+                            }
+                        }
+
+                        _waypoint.X = _ship.X + delta.X;
+                        _waypoint.Y = _ship.Y + delta.Y;
                     }
-                    _xWaypoint = _x + rdx;
-                    _yWaypoint = _y + rdy;
+                    // Change the ship's direction
+                    else
+                    {
+                        _facing = GetNextDirection(_facing, value / 90, direction == 'R');
+                    }
                     break;
             }
         }
-    }
-    
-    public class Action
-    {
-        public char Direction;
-        public int Value;
+
+        private static char GetNextDirection(char current, int turns, bool clockwise)
+        {
+            for (int i = 1; i <= turns; i++)
+            {
+                current = current switch
+                {
+                    'N' => clockwise ? 'E' : 'W',
+                    'E' => clockwise ? 'S' : 'N',
+                    'S' => clockwise ? 'W' : 'E',
+                    'W' => clockwise ? 'N' : 'S',
+                    _ => current
+                };
+            }
+
+            return current;
+        }
     }
 }
