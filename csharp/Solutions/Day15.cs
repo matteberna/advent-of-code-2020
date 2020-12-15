3afa3945b5a1fc9d658a8076b4a2b85e900ef57c
Matteo Bernardini <watteberna@gmail.com>
2020-12-15 10:10:12 +0100

    Solve the second part of day 15

--- csharp/Solutions/Day15.cs
+++ csharp/Solutions/Day15.cs
@@ -1,63 +1,54 @@
-using System.Linq;
+using System.Collections.Generic;
 
 namespace Whiskee.AdventOfCode2020.Solutions
 {
     public class Day15 : Day
     {
-        private static int?[] _spoken;
-        private static int _head = -1;
+        private static Dictionary<int, int> _initial;
+        private static int _initialLast = 0;
 
         public override void ReadInput(string content)
         {
             string[] numbers = content.Split(",");
-            _spoken = new int?[2020];
-            for (int i = 0; i < 2020; i++)
+            _initial = new Dictionary<int, int>();
+            for (int i = 0; i < numbers.Length; i++)
             {
-                if (i < numbers.Length)
-                {
-                    _spoken[i] = int.Parse(numbers[i]);
-                    _head++;
-                }
-                else
-                {
-                    _spoken[i] = null;
-                }
+                _initial.Add(int.Parse(numbers[i]), i + 1);
+                _initialLast = int.Parse(numbers[i]);
             }
         }
 
         public override object SolveFirst()
         {
-            int last = (int)_spoken[_head]!;
-            for (int t = _head; t < 2019; t++)
-            {
-                if (_spoken.Count(s => s == last) == 1)
-                {
-                    last = 0;
-                }
-                else
-                {
-                    int age = 0;
-                    for (int i = _head - 1; i >= 0; i--)
-                    {
-                        age++;
-                        if (_spoken[i] == last)
-                        {
-                            last = age;
-                            break;
-                        }
-                    }
-                }
-                
-                _head++;
-                _spoken[_head] = last;
-            }
-            
-            return _spoken[2019];
+            return SolveFor(2020);
         }
 
         public override object SolveSecond()
         {
-            return null;
+            return SolveFor(30000000);
+        }
+
+        private static int? SolveFor(int limit)
+        {
+            var dict = new Dictionary<int, int>(_initial);
+            int last = _initialLast;
+            for (int turn = _initial.Count + 1; turn <= limit; turn++)
+            {
+                int say;
+                if (dict.ContainsKey(last))
+                {
+                    say = turn == _initial.Count + 1? 0 : turn - 1 - dict[last];
+                    dict[last] = turn - 1;
+                }
+                else
+                {
+                    say = 0;
+                    dict[last] = turn - 1;
+                }
+                last = say;
+            }
+            
+            return last;
         }
     }
 }
